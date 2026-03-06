c6be3e1cd7e24203deef16d9cee756ccb64e5033
jose25henrique.ramos2019 <jose25henrique.ramos2019@gmail.com>
2026-03-06 23:07:58 +0000

    0.2-a

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      // Redireciona requisições de /api para a API da Omie
       '/api': {
         target: 'https://app.omie.com.br',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        // A regra 'rewrite' foi removida. O caminho /api é necessário
+        // tanto para acionar o proxy quanto na URL de destino da Omie.
       }
     }
   }
